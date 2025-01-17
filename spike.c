eb498c217810063bde007c85b0a4c3e16dae6744
Dean Netherton <dinoboards@netherton.org>
2025-01-17 21:25:01 +1100

    fixed issue with error on basic script completion

--- spike.c
+++ spike.c
@@ -5,18 +5,55 @@
 extern uint24_t himem;
 extern uint8_t ACCS[256];
 extern uint8_t OPTVAL;
+extern uint8_t* TABLE[8];
 
 void debug() {
   printf("HIMEM: %X\r\n", himem);
+  // printf("TABLE: %p\r\n", TABLE);
+  // for(int i = 0; i < 8; i++) {
+  //   printf("  TABLE[%d]: %p\r\n", i, TABLE[i]);
+  // }
 }
 
-void abort_bdos1() {
-  printf("Abort BDOS1\r\n");
+#define ABORT_X(name) void abort_##name(uint24_t af, uint24_t bc, uint24_t de, uint8_t *hl) { \
+  printf("Abort " #name ".  AF:%X, BC: %X, DE: %X, HL: %p (*HL): %x\r\n", af, bc, de, hl, *hl); \
+  abort();\
+}
+
+ABORT_X(read)
+ABORT_X(write)
+ABORT_X(exists)
+ABORT_X(dir1)
+ABORT_X(x1)
+
+
+void abort_bdos1(uint24_t af, uint24_t bc, uint24_t de, uint24_t hl) {
+  printf("Abort BDOS1.  AF:%X, BC: %X, DE: %X, HL: %X\r\n", af, bc, de, hl);
   abort();
 }
 
-void abort_setdma() {
-  printf("Abort SETDMA\r\n");
+void abort_bdos16(uint24_t af, uint24_t bc, uint24_t de, uint24_t hl) {
+  printf("Abort BDOS16.  AF:%X, BC: %X, DE: %X, HL: %X\r\n", af, bc, de, hl);
+  abort();
+}
+
+void abort_osshut(uint24_t af, uint24_t bc, uint24_t de, uint24_t hl) {
+  printf("Abort OSSHUT.  AF:%X, BC: %X, DE: %X, HL: %X\r\n", af, bc, de, hl);
+  abort();
+}
+
+void abort_seshut(uint24_t af, uint24_t bc, uint24_t de, uint24_t hl) {
+  printf("Abort SESHUT.  AF:%X, BC: %X, DE: %X, HL: %X\r\n", af, bc, de, hl);
+  abort();
+}
+
+void abort_stload(uint24_t af, uint24_t bc, uint24_t de, uint24_t hl) {
+  printf("Abort STLOAD.  AF:%X, BC: %X, DE: %X, HL: %X\r\n", af, bc, de, hl);
+  abort();
+}
+
+void abort_setdma(uint24_t af, uint24_t bc, uint24_t de, uint24_t hl) {
+  printf("Abort SETDMA.  AF:%X, BC: %X, DE: %X, HL: %X\r\n", af, bc, de, hl);
   abort();
 }
 
