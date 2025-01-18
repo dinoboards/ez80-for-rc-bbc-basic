287634355bd87c4d901850dd139ad6e5f2962ddd
Dean Netherton <dinoboards@netherton.org>
2025-01-18 17:19:14 +1100

    file open/close writing confirmed, including spooling

--- spike.c
+++ spike.c
@@ -14,22 +14,25 @@ extern uint8_t RAM_END[];
 extern uint8_t end_of_bss[];
 extern uint8_t _heap[];
 extern uint32_t STAVAR[27];
+extern uint24_t FREE;
+extern uint8_t FCB_BLOCKS[];
+extern uint24_t FREE_FCB_TABLE;
 
 uint24_t debug() {
   printf("HIMEM: %X\r\n", himem);
+  printf("FREE: %X\r\n", FREE);
   printf("FCB: %p\r\n", CPM_SYS_FCB);
   printf("DISK_BUFFER: %p\r\n", DISK_BUFFER);
   printf("OPTVAL: %p, %x\r\n", &OPTVAL, OPTVAL);
   printf("TRPCNT: %p, %x\r\n", &TRPCNT, TRPCNT);
   printf("RAM_END: %p\r\n", RAM_END);
-  // printf("USER: %p, %p\r\n", &USER, *USER);
   printf("end_of_bss: %p\r\n", end_of_bss);
   printf("_heap: %p\r\n", _heap);
   printf("STAVAR: %p, %lX, %lX, %lX\r\n", STAVAR, STAVAR[0], STAVAR[1], STAVAR[2]);
+  printf("FREE_FCB_TABLE: %X\r\n", FREE_FCB_TABLE);
 
   uint24_t heap_aligned = ((uint24_t)_heap + 255) & ~255;
   printf("heap_aligned: %x\r\n", heap_aligned);
-
 
   printf("TABLE: %p\r\n", TABLE);
   for(int i = 0; i < 8; i++) {
@@ -39,8 +42,12 @@ uint24_t debug() {
   return heap_aligned;
 }
 
-void log_info(const char* name, uint24_t af, uint24_t bc, uint24_t de, uint24_t hl, uint24_t ix) { \
-  printf("log: %s.  AF:%X, BC: %X, DE: %X, HL: %X, ix: %X\r\n", name, af, bc, de, hl, ix); \
+void log_info(const char* name, uint24_t af, uint24_t bc, uint24_t de, uint24_t hl, uint24_t ix, uint24_t iy) { \
+  printf("log: %s.  AF:%X, BC: %X, DE: %X, HL: %X, ix: %X, iy: %X\r\n", name, af, bc, de, hl, ix, iy); \
+  printf("FREE_FCB_TABLE: %X\r\n", FREE_FCB_TABLE);
+  for(int i = 0; i < 8; i++) {
+    printf("  TABLE[%d](%p): %p\r\n", i, &TABLE[i], TABLE[i]);
+  }
 }
 
 #define ABORT_X(name) void abort_##name(uint24_t af, uint24_t bc, uint24_t de, uint24_t hl, uint24_t ix) { \
